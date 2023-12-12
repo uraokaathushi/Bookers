1b7369f1b37e27e4eaf22cd893f38b1b8ee1246b
uraokaathushi <149951267+uraokaathushi@users.noreply.github.com>
2023-12-12 23:26:18 +0000

    [start]課題2-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get '' => 'homes#top'
-  get 'books' => 'books#index'
-  get 'books' => 'books#new'
+  root :to => 'homes#top'
+  get 'books/new'
+  get 'books' => 'books#index', as: 'index_book'
   post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show',as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
